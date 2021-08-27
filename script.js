9c8c0258a55705bf5db9ae01eb0680a188a7ec47
lmorandiz <89102139+lmorandiz@users.noreply.github.com>
2021-08-27 17:51:37 -0300

    Proyecto final LM

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+    if (amount <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } else {
+        document.getElementById("result").innerHTML="If you deposit "
+            +"<span class=\"result\">" +principal + "</span>"+",\<br\>at an interest rate of "
+            +"<span class=\"result\">" +rate + "%</span>"+"\<br\>You will receive an amount of "
+            +"<span class=\"result\">" +amount + "</span>"+",\<br\>in the year "
+            +"<span class=\"result\">" +year + "</span>"+"\<br\>"
+    }
 }
-        
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + '%';
+}
